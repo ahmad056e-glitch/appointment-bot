be176ca82e3e67433009bdcd0f60623a6cb1378f
ahmad056e-glitch <alihamza056e@gmail.com>
2026-05-11 22:29:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const INTERVAL = 5 * 60 * 1000; // 5 minutes
 
 async function runAllChecks() {
   console.log(`[${new Date().toISOString()}] Running all checks...`);
-  await checkSpain();
-  await checkGermany();
-  await checkItaly();
+  if (process.env.SPAIN_USERNAME) await checkSpain();
+  if (process.env.GERMANY_USERNAME) await checkGermany();
+  if (process.env.ITALY_USERNAME) await checkItaly();
 }
 
 runAllChecks();
